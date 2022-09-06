f81b37f61b96f2905efc9dc36900b936bd19fc48
Vikas Meena <87318981+vikasme18@users.noreply.github.com>
2022-09-06 20:42:55 +0530

    Update PositiveNegativeProgram.java

--- Day 01-09-2022/PositiveNegativeProgram.java
+++ Day 01-09-2022/PositiveNegativeProgram.java
@@ -1,13 +1,17 @@
+// This program checks whether the given number is negative, positive or zero.
 import java.util.Scanner;
 
 class PositiveNegativeProgram {
-
+	// Calling main method
 	public static void main(String[] args) {
+		
 		// Creating scanner object
 		Scanner scan = new Scanner(System.in);
+		
+		// Taking user input
 		System.out.print("Enter any number: ");
 		int num = scan.nextInt();
-
+		
 		if (num<0) {
 			System.out.print("Negative number");
 		}
