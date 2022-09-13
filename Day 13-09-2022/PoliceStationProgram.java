14e01281157c2a3f8f401810efd78cffb5e52c41
Vikas Meena <87318981+vikasme18@users.noreply.github.com>
2022-09-13 16:03:45 +0530

    Update PoliceStationProgram.java

--- Day 13-09-2022/PoliceStationProgram.java
+++ Day 13-09-2022/PoliceStationProgram.java
@@ -1,27 +1,37 @@
 class PoliceStationProgram {
-
+	
+	// This method will print only pincode
 	void Area(int pincode) {
 		System.out.println(pincode);
 	}
 	
+	// This method will print pincode and police station
 	void Area(int pincode, String police_s) {
 		System.out.println(pincode);
 		System.out.println(police_s);
 	}
-
+	
+	// This method will print pincode, police station and city
 	void Area(int pincode, String police_s, String city) {
 		System.out.println(pincode);
 		System.out.println(police_s);
 		System.out.println(city);
 	}	
-
+	
+	// Calling main method
 	public static void main(String[] args) {
+		
+		// Scanner class object
 		java.util.Scanner scan = new java.util.Scanner(System.in);
+		
+		// Taking input from the user and storing inside num_data
 		System.out.print("How many data you want to enter: ");
 		int num_data = scan.nextInt();
 		
+		// Creating a object of PoliceStationProgram class
 		PoliceStationProgram ps1 =  new PoliceStationProgram();
 		
+		// Checking the number of data using switch statement
 		switch(num_data) {
 			case 1:
 				ps1.Area(421306);
