9dcd01eaa195d32c5cdb45fc09b5f09c43076258
Vikas Meena <87318981+vikasme18@users.noreply.github.com>
2022-09-13 18:42:40 +0530

    Add files via upload

---
+++ Day 13-09-2022/PerfectNumber.java
@@ -0,0 +1,24 @@
+public class PerfectNumber {
+
+	void findNumber(int limit, int u) {
+		int num_count = 0, sum = 0;
+		for (int i = limit; i <= u; i++) {
+			for (int j = 1; j < i; j++) {
+				if (i % j == 0) {
+					sum += j;
+				}
+			}
+			if (sum == i) {
+				num_count += 1;
+			}
+			sum = 0;
+		}
+		
+		System.out.print("Total number of perfect: " + num_count);
+	}
+	
+	public static void main(String[] args) {
+		PerfectNumber pnum1 = new PerfectNumber();
+		pnum1.findNumber(1, 30);
+	}
+}
