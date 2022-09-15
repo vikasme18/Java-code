d29d39401fe5973c335edab429d96539c94bc60f
Vikas Meena <87318981+vikasme18@users.noreply.github.com>
2022-09-15 19:35:38 +0530

    Update ConstructorProgram.java

--- Lab 15-09-2022/ConstructorProgram.java
+++ Lab 15-09-2022/ConstructorProgram.java
@@ -2,35 +2,43 @@ import java.util.Scanner;
 
 public class ConstructorProgram {
 	
+	// non static student variable
 	int id;
 	String name;
 	String location;
 	
+	// This method will take single agruments
 	ConstructorProgram(int id) {
 		this.id = id;
 	}
 	
+	// This method will take two agruments
 	ConstructorProgram(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
+	// This method will take three agruments
 	ConstructorProgram(int id, String name, String location) {
 		this.id = id;
 		this.name = name;
 		this.location = location;
 	}
 	
+	// THis method display details of student
 	void showInfo() {
 		System.out.println("ID: " + id);
 		System.out.println("Name: " + name);
 		System.out.println("location: " + location);
 		System.out.println();
 	}
-
+	
+	// calling main method
 	public static void main(String[] args) {
+		// scanner object
 		Scanner scan = new Scanner(System.in);
 		
+		// creating objects
 		ConstructorProgram emp1 = new ConstructorProgram(001);
 		ConstructorProgram emp2 = new ConstructorProgram(002, "Vikas");
 		ConstructorProgram emp3 = new ConstructorProgram(003, "Ashutosh", "Murbad");
